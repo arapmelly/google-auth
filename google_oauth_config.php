4574658acaf3399b67b725e632360d33e393f9ec
Jacob Chumo <002028@scangroup.local>
2019-08-20 14:51:28 +0300

    changes

---
+++ google_oauth_config.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+//Google API PHP Library includes
+require_once 'vendor/autoload.php';
+
+// Set config params to acces Google API
+ $client_id = '214940814253-fd11ba7nje065ncau0srk2p2keam69ea.apps.googleusercontent.com';
+ $client_secret = 'AIzaSyBGK442K-SxJ-5ChVV1bdIy1fMvQe4Y9Qw';
+ $redirect_uri = 'http://social-auth.pocktt.com';
+ 
+//Create and Request to access Google API
+$client = new Google_Client();
+$client->setApplicationName("Google OAuth");
+$client->setClientId($client_id);
+$client->setClientSecret($client_secret);
+$client->setRedirectUri($redirect_uri);
+$client->addScope("https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile");
+
+$objRes = new Google_Service_Oauth2($client);
+
+//Add access token to php session after successfully authenticate
+if (isset($_GET['code'])) {
+  $client->authenticate($_GET['code']);
+  $_SESSION['access_token'] = $client->getAccessToken();
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
+
+//set token
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client->setAccessToken($_SESSION['access_token']);
+}
+
+//store with user data
+if ($client->getAccessToken()) {
+  $userData = $objRes->userinfo->get();
+  if(!empty($userData)) {
+	//insert data into database
+  }
+  $_SESSION['access_token'] = $client->getAccessToken();
+} else {
+  $googleAuthUrl  =  $client->createAuthUrl();
+}
+?>
